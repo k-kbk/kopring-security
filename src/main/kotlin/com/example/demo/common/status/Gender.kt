b7e8442351f2a72f2f0e74bd2bbdb43221678870
k-kbk <kkbk0077@gmail.com>
2024-01-02 13:25:36 +0900

    feat: 인증 구현

--- src/main/kotlin/com/example/demo/common/status/Gender.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo.common.status
-
-enum class Gender(val desc: String) {
-    MAN("남자"),
-    WOMAN("여자")
-}
--- src/main/kotlin/com/example/demo/member/repository/MemberRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demo.member.repository
-
-import com.example.demo.member.entity.Member
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MemberRepository : JpaRepository<Member, Long> {
-    fun findByLoginId(loginId: String): Member?
-}
